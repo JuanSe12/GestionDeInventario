c70fa5f85871b7496a99ecbb25c8fb902fc61360
juanse <megajuan_45@hotmail.com>
2014-05-28 21:06:56 -0500

    Java-proyecto

---
+++ src/Control/Entrada_Producto.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Control;
+
+/**
+ *
+ * @author adsi
+ */
+public class Entrada_Producto {
+   int nrm_producto_e;
+   String fch_entrada; 
+
+    public int getNrm_producto_e() {
+        return nrm_producto_e;
+    }
+
+    public void setNrm_producto_e(int nrm_producto_e) {
+        this.nrm_producto_e = nrm_producto_e;
+    }
+
+    public String getFch_entrada() {
+        return fch_entrada;
+    }
+
+    public void setFch_entrada(String fch_entrada) {
+        this.fch_entrada = fch_entrada;
+    }
+    public void registrar_entrada(){
+        
+    }
+}
